b3115824cdeb923102f8ca491f4ce4045a7990d8
kevin <kevin@appledeMacBook-Pro.local>
2022-06-13 21:40:27 +0800

    1st commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Kevin on 2022/3/15.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
